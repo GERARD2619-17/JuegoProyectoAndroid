8913ab221c7307c21c40a1cc2f57fcad0fd6c8fb
kevin <kammonroy@gmail.com>
2020-09-29 22:13:10 -0600

    no sirvee

--- app/src/main/java/com/example/juego/MainActivity.java
+++ app/src/main/java/com/example/juego/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 //hola esto es una prueba
+    //NO SIRVEEEEEEEE!!!!!!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
