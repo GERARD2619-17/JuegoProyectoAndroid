026cc936bc719eb34ce4f9ea004ffbf1d7b4de38
Gerardo Zelaya <57957674+GERARD2619-17@users.noreply.github.com>
2020-09-29 22:06:39 -0600

    prueba de commit de nuestro juego android

--- app/src/main/java/com/example/juego/MainActivity.java
+++ app/src/main/java/com/example/juego/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+//hola esto es una prueba
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
