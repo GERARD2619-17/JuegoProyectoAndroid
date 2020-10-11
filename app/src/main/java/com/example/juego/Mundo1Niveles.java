ed79a35f356688814600e6d239125ea5587d86e3
kevin <kammonroy@gmail.com>
2020-10-11 13:55:48 -0600

    temporizador en el cambio de turno

--- app/src/main/java/com/example/juego/Mundo1Niveles.java
+++ app/src/main/java/com/example/juego/Mundo1Niveles.java
@@ -17,24 +17,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mundo1Niveles extends AppCompatActivity implements View.OnClickListener{
-
     private List<Grupo> grupos = new ArrayList<>();
     private List<ImageButton> botones = new ArrayList<>();
     private int turno=1;
     private int NumeroEnJuego=0;
-    private int turnos;
-    private int Nivel;
-    private static int contador;
+    private static int contador=0;
     private Handler Correr;
+    private TextView txt;
+    private boolean incrementando=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mundo1_niveles);
-        turnos = 0;
-        Nivel = Integer.parseInt(getIntent().getStringExtra("nivel"));
+        this.Correr = new Handler();
         agregarBotones();
-        cargarNivel();
+        cargarGrupos();
         cargar();
+        this.txt = findViewById(R.id.txtContador);
     }
     //Carga e inicaliza los botones, en un metodo y luego los agrega en una lista "botones" para acceder a ellos cuando querramos
     private void agregarBotones(){
@@ -144,11 +143,24 @@ public class Mundo1Niveles extends AppCompatActivity implements View.OnClickList
         btn34.setOnClickListener(this);
         btn35.setOnClickListener(this);
     }
-    //Carga el nivel seleccionado
-    private void cargarNivel(){
-        NivelesMundo1 nivel = new NivelesMundo1(Nivel);
-        grupos = nivel.getNivelesMundo1();
+    //Genera los valores aleatorios de nuestras piezas en el tablero y los agrega en la lista "grupos" para acceder a ellos cuando querramos
+    private void cargarGrupos(){
+        //NivelesMundo1 nivel = new NivelesMundo1(Nivel);
+        //grupos = nivel.getNivelesMundo1();
+        int contador = 0;
+        for(int i = 1; i<=7; i++){
+            for(int j = 1; j <=5; j++){
+                int id;
+                if(porcentaje(12)) id=0;
+                else id = (int)(Math.random() * 2) + 1;
+                int cantidad = (int)(Math.random() * 6) + 1;
+                Grupo g = new Grupo(id,i,j,cantidad,contador);
+                grupos.add(g);
+                contador++;
+            }
+        }
     }
+    //Carga e inicaliza los botones, en un metodo y luego los agrega en una lista "botones" para acceder a ellos cuando querramos
     //Carga las piezas en el tablero
     private void cargar(){
         for (int i=0;i<35;i++){
@@ -270,120 +282,125 @@ public class Mundo1Niveles extends AppCompatActivity implements View.OnClickList
         }
         return retorno;
     }
-
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.btn1:
-                Jugar(0);
-                break;
-            case R.id.btn2:
-                Jugar(1);
-                break;
-            case R.id.btn3:
-                Jugar(2);
-                break;
-            case R.id.btn4:
-                Jugar(3);
-                break;
-            case R.id.btn5:
-                Jugar(4);
-                break;
-            case R.id.btn6:
-                Jugar(5);
-                break;
-            case R.id.btn7:
-                Jugar(6);
-                break;
-            case R.id.btn8:
-                Jugar(7);
-                break;
-            case R.id.btn9:
-                Jugar(8);
-                break;
-            case R.id.btn10:
-                Jugar(9);
-                break;
-            case R.id.btn11:
-                Jugar(10);
-                break;
-            case R.id.btn12:
-                Jugar(11);
-                break;
-            case R.id.btn13:
-                Jugar(12);
-                break;
-            case R.id.btn14:
-                Jugar(13);
-                break;
-            case R.id.btn15:
-                Jugar(14);
-                break;
-            case R.id.btn16:
-                Jugar(15);
-                break;
-            case R.id.btn17:
-                Jugar(16);
-                break;
-            case R.id.btn18:
-                Jugar(17);
-                break;
-            case R.id.btn19:
-                Jugar(18);
-                break;
-            case R.id.btn20:
-                Jugar(19);
-                break;
-            case R.id.btn21:
-                Jugar(20);
-                break;
-            case R.id.btn22:
-                Jugar(21);
-                break;
-            case R.id.btn23:
-                Jugar(22);
-                break;
-            case R.id.btn24:
-                Jugar(23);
-                break;
-            case R.id.btn25:
-                Jugar(24);
-                break;
-            case R.id.btn26:
-                Jugar(25);
-                break;
-            case R.id.btn27:
-                Jugar(26);
-                break;
-            case R.id.btn28:
-                Jugar(27);
-                break;
-            case R.id.btn29:
-                Jugar(28);
-                break;
-            case R.id.btn30:
-                Jugar(29);
-                break;
-            case R.id.btn31:
-                Jugar(30);
-                break;
-            case R.id.btn32:
-                Jugar(31);
-                break;
-            case R.id.btn33:
-                Jugar(32);
-                break;
-            case R.id.btn34:
-                Jugar(33);
-                break;
-            case R.id.btn35:
-                Jugar(34);
-                break;
+        if(incrementando==false){
+            switch (view.getId()){
+                case R.id.btn1:
+                    Jugar(0);
+                    break;
+                case R.id.btn2:
+                    Jugar(1);
+                    break;
+                case R.id.btn3:
+                    Jugar(2);
+                    break;
+                case R.id.btn4:
+                    Jugar(3);
+                    break;
+                case R.id.btn5:
+                    Jugar(4);
+                    break;
+                case R.id.btn6:
+                    Jugar(5);
+                    break;
+                case R.id.btn7:
+                    Jugar(6);
+                    break;
+                case R.id.btn8:
+                    Jugar(7);
+                    break;
+                case R.id.btn9:
+                    Jugar(8);
+                    break;
+                case R.id.btn10:
+                    Jugar(9);
+                    break;
+                case R.id.btn11:
+                    Jugar(10);
+                    break;
+                case R.id.btn12:
+                    Jugar(11);
+                    break;
+                case R.id.btn13:
+                    Jugar(12);
+                    break;
+                case R.id.btn14:
+                    Jugar(13);
+                    break;
+                case R.id.btn15:
+                    Jugar(14);
+                    break;
+                case R.id.btn16:
+                    Jugar(15);
+                    break;
+                case R.id.btn17:
+                    Jugar(16);
+                    break;
+                case R.id.btn18:
+                    Jugar(17);
+                    break;
+                case R.id.btn19:
+                    Jugar(18);
+                    break;
+                case R.id.btn20:
+                    Jugar(19);
+                    break;
+                case R.id.btn21:
+                    Jugar(20);
+                    break;
+                case R.id.btn22:
+                    Jugar(21);
+                    break;
+                case R.id.btn23:
+                    Jugar(22);
+                    break;
+                case R.id.btn24:
+                    Jugar(23);
+                    break;
+                case R.id.btn25:
+                    Jugar(24);
+                    break;
+                case R.id.btn26:
+                    Jugar(25);
+                    break;
+                case R.id.btn27:
+                    Jugar(26);
+                    break;
+                case R.id.btn28:
+                    Jugar(27);
+                    break;
+                case R.id.btn29:
+                    Jugar(28);
+                    break;
+                case R.id.btn30:
+                    Jugar(29);
+                    break;
+                case R.id.btn31:
+                    Jugar(30);
+                    break;
+                case R.id.btn32:
+                    Jugar(31);
+                    break;
+                case R.id.btn33:
+                    Jugar(32);
+                    break;
+                case R.id.btn34:
+                    Jugar(33);
+                    break;
+                case R.id.btn35:
+                    Jugar(34);
+                    break;
+            }
         }
+
     }
     //Boton TERMINAR TURNO
     public void Turno_onClick(View v){
-        Incrementar();
+        incrementando=true;
+        //Incrementar();
+        new Thread(new cargaLenta()).start();
+        contador = 0;
         if(turno==1) {
             turno=2;
         }
@@ -406,18 +423,22 @@ public class Mundo1Niveles extends AppCompatActivity implements View.OnClickList
         if(x>1 && getGrupo(x-1,y).getId()!=0 && getGrupo(x,y).getId()!=0 && getGrupo(x,y).getCantidad()!=1 && getGrupo(x-1,y).getId()!=getGrupo(x,y).getId() && getGrupo(x,y).getId()==turno){
             grupos.get(getGrupo(x-1,y).getNumero()).setEstado(1);
             ImagenBoton(getGrupo(x-1,y).getNumero(),getGrupo(x-1,y).getCantidad(),2);
+            //botones.get(getGrupo(x-1,y).getNumero()).setBackgroundColor(Color.rgb(0,0,255));
         }
         if(x<7 && getGrupo(x+1,y).getId()!=0 && getGrupo(x,y).getId()!=0 && getGrupo(x,y).getCantidad()!=1 && getGrupo(x+1,y).getId()!=getGrupo(x,y).getId() && getGrupo(x,y).getId()==turno){
             grupos.get(getGrupo(x+1,y).getNumero()).setEstado(1);
             ImagenBoton(getGrupo(x+1,y).getNumero(),getGrupo(x+1,y).getCantidad(),2);
+            //botones.get(getGrupo(x+1,y).getNumero()).setBackgroundColor(Color.rgb(0,0,255));
         }
         if(y>1 && getGrupo(x,y-1).getId()!=0 && getGrupo(x,y).getId()!=0 && getGrupo(x,y).getCantidad()!=1 && getGrupo(x,y-1).getId()!=getGrupo(x,y).getId() && getGrupo(x,y).getId()==turno){
             grupos.get(getGrupo(x,y-1).getNumero()).setEstado(1);
             ImagenBoton(getGrupo(x,y-1).getNumero(),getGrupo(x,y-1).getCantidad(),2);
+            //botones.get(getGrupo(x,y-1).getNumero()).setBackgroundColor(Color.rgb(0,0,255));
         }
         if(y<5 && getGrupo(x,y+1).getId()!=0 && getGrupo(x,y).getId()!=0 && getGrupo(x,y).getCantidad()!=1 && getGrupo(x,y+1).getId()!=getGrupo(x,y).getId() && getGrupo(x,y).getId()==turno){
             grupos.get(getGrupo(x,y+1).getNumero()).setEstado(1);
             ImagenBoton(getGrupo(x,y+1).getNumero(),getGrupo(x,y+1).getCantidad(),2);
+            //botones.get(getGrupo(x,y+1).getNumero()).setBackgroundColor(Color.rgb(0,0,255));
         }
         grupos.get(getGrupo(x,y).getNumero()).setEstado(2);
     }
@@ -454,12 +475,12 @@ public class Mundo1Niveles extends AppCompatActivity implements View.OnClickList
 
         if(Atacante.getCantidad() > Atacado.getCantidad()){
             //Gana Atacante
-            Ganador = Atacante.getCantidad() - Atacado.getCantidad()-1;
+            Ganador = Atacante.getCantidad() - Atacado.getCantidad();
             grupos.get(n1).setId(grupos.get(n2).getId());
             grupos.get(n1).setCantidad(Ganador);
         }else if(Atacado.getCantidad() > Atacante.getCantidad()){
             //Gana Atacado
-            Ganador = Atacado.getCantidad() - Atacante.getCantidad()+1;
+            Ganador = Atacado.getCantidad() - Atacante.getCantidad();
             grupos.get(n1).setCantidad(Ganador);
         }
         else {
@@ -472,52 +493,64 @@ public class Mundo1Niveles extends AppCompatActivity implements View.OnClickList
         }
         grupos.get(n2).setCantidad(1);
     }
-    //Incrementa las tropas en cada celda
-    private void Incrementar(){
-        if(turnos<12){
-            turnos++;
-        }
+
+    private List<Integer> celdasAfectadas(){
         //Saco la lista de los numeros de las celdas a alterar y la almaceno en el arreglo "celdas"
         List<Integer> celdas = new ArrayList<>();
-        if(turno==1){
+        if(turno==2){
             for(int i=0 ;i<grupos.size();i++){
-                if(grupos.get(i).getId()==1){
+                if(grupos.get(i).getId()==1 && grupos.get(i).getCantidad()<6){
                     celdas.add(grupos.get(i).getNumero());
                 }
             }
         }
         else{
             for(int i=0 ;i<grupos.size();i++){
-                if(grupos.get(i).getId()==2){
+                if(grupos.get(i).getId()==2 && grupos.get(i).getCantidad()<6){
                     celdas.add(grupos.get(i).getNumero());
                 }
             }
         }
-        //Tomo el arreglo "celdas" e incremento
-        for(int i=0; i<celdas.size();i++){
-            //Si la celda es mayor a 6
-            if(grupos.get(celdas.get(i)).getCantidad()<6){
-                //tengo el 90% de incrementar su valor
-                if(porcentaje(90)){
-                    grupos.get(celdas.get(i)).setCantidad(grupos.get(celdas.get(i)).getCantidad()+1);
-                }
-            }
-            cargar();
-        }
+        return celdas;
     }
-
-    final class Maquina implements  Runnable{
+    //Incrementa las tropas en cada celda
+    final class cargaLenta implements  Runnable{
+        int n=0;
         @Override
         public void run() {
-            while(contador< 10){
+            while(contador< celdasAfectadas().size()-1){
                 metodoEspera();
                 Correr.post(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(Mundo1Niveles.this, String.valueOf(contador), Toast.LENGTH_SHORT).show();
-
-                        if(contador==100) {
-
+                        txt.setText(String.valueOf(n+1));
+                        contador=n;
+                        List<Integer> celdas = new ArrayList<>();
+                        if(turno==2){
+                            for(int i=0 ;i<grupos.size();i++){
+                                if(grupos.get(i).getId()==1 && grupos.get(i).getCantidad()<6){
+                                    celdas.add(grupos.get(i).getNumero());
+                                }
+                            }
+                        }
+                        else{
+                            for(int i=0 ;i<grupos.size();i++){
+                                if(grupos.get(i).getId()==2 && grupos.get(i).getCantidad()<6){
+                                    celdas.add(grupos.get(i).getNumero());
+                                }
+                            }
+                        }
+                        //Si la celda es mayor a 6
+                        if(grupos.get(celdas.get(n)).getCantidad()<6){
+                            //tengo el 90% de incrementar su valor
+                            if(porcentaje(90)){
+                                grupos.get(celdas.get(n)).setCantidad(grupos.get(celdas.get(n)).getCantidad()+1);
+                            }
+                        }
+                        n++;
+                        cargar();
+                        if(n==celdasAfectadas().size()){
+                            incrementando=false;
                         }
                     }
                 });
@@ -525,12 +558,13 @@ public class Mundo1Niveles extends AppCompatActivity implements View.OnClickList
         }
         private void metodoEspera() {
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
                 contador++;
             }catch (Exception e ){
 
             }
 
         }
+
     }
 }
